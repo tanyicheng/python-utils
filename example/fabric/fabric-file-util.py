e8900cace39114913dee52698cb97f0f54cba03e
barrett <yicheng.tan@seraphim-energy.com>
2021-02-18 16:51:01 +0800

    数据库备份+下载更新

--- example/fabric/fabric-file-util.py
+++ example/fabric/fabric-file-util.py
@@ -22,26 +22,34 @@ def get_file():
 
     # run('ls')
 
+
 # 上传文件
 def upload():
     put(r'D:\logs\service_log.txt', '/home/')
 
 
+database_path = '/home/backup/jeecg-boot.sql'
+
+
 # 下载文件
 def download():
     # 进入目录且。。。
-    with cd('/home'):
-        get('jeecg-boot.sql', r'D:\logs')
+    get(database_path, r'D:\1_sql\backup')
 
 
 # 备份远程服务器下 docker 环境的mysql数据库
 def mysql_bak():
     # result = run('mysqldump -u root -p123456 jeecg-boot > /home/jeecg-boot.sql')
-    result = run('docker exec -it mysql mysqldump -u root -p123456 jeecg-boot > /home/jeecg-boot.sql')
-    print("result："+result)
+    result = run('docker exec -it mysql mysqldump -u root -p123456 jeecg-boot > ' + database_path)
+    print("result：" + result)
+
+
+def do():
+    mysql_bak()
+    download()
 
 
 # 执行命令，默认是在~目录下
 # >fab -f fabric01.py deploy 或 execute(deploy)
 # execute(download)
-execute(mysql_bak)
+execute(do)
