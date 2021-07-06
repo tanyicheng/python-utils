633c5db4078e2e9294e67244da4225885849b7b0
barrett <yicheng.tan@seraphim-energy.com>
2021-07-06 22:26:38 +0800

    mysql 同步数据初步构建

--- example/mysql/database_bak_ok.py
+++ example/mysql/database_bak_ok.py
@@ -10,21 +10,28 @@ env.password = '123456'
 
 currentTime = time.strftime("%Y%m%d%H%M%S", time.localtime())
 # 备份数据库且下载的本地
-database_path = '/home/backup/jeecg-boot'+currentTime+'.sql'
+database_path = '/home/backup/jeecg-boot' + currentTime + '.sql'
+# TODO jeecg 系统数据备份
+# local_path = 'D:\1_sql\backup'
+local_path = 'D:\git\my_study\jeecg-boot\jeecg-boot\db'
 
 
 # 下载文件
 def download():
     # 进入目录且。。。
-    get(database_path, r'D:\1_sql\backup')
+    get(database_path, r'' + local_path)
 
 
-# 备份远程服务器下 docker 环境的mysql数据库
+# 备份远程服务器下 docker 环境的mysql数据库，TODO 注意不能通过Navicat导入，数据库名导入时也需要一致
 def mysql_bak():
     # result = run('mysqldump -u root -p123456 jeecg-boot > /home/jeecg-boot.sql')
-    result = run('docker exec -it mysql mysqldump -u root -p123456 jeecg-boot > ' + database_path)
+    # result = run('docker exec -it mysql mysqldump -uroot -p123456 -h127.0.0.1 -P3306 --databases jeecg-boot > ' + database_path)
+    result = run('docker exec -it mysql mysqldump -uroot -p123456 jeecg-boot > ' + database_path)
     print("result：" + result)
 
+# linux 测试可行
+# mysqldump -uroot -p123456 --databases jeecg-boot> jeecgboot.bak.sql
+# mysql -uroot -p123456  jeecg-boot<jeecgboot.bak.sql
 
 def do():
     mysql_bak()
--- ser-dev-ops/mysqlbinlog-bak.py
+++ ser-dev-ops/mysqlbinlog-bak.py
@@ -8,26 +8,41 @@ yaml.warnings({'YAMLLoadWarning': False})
 f = open('conf/linux.yaml', 'r', encoding='utf-8')
 cfg = f.read()
 d = yaml.load(cfg)
-hosts = d.get('linux32').get('hosts')
+hosts = d.get('linux44').get('hosts')
 # password = d.get('linux32').get('password')
 
-#todo 放弃，用scp处理
 # 全局变量 root@192.168.91.100:22
-env.hosts = ['root@172.16.8.32:22']
+env.hosts = [hosts]
 # env.password = password
-#使用证书登录
-env.key_filename ='D:\\0-seraphim\\1seraphim-证书'
+# 使用证书登录
+env.key_filename = 'D:\\0-seraphim\\1seraphim-证书'
 
 
-def workask(dirname):
+def dir_ls(dirname):
     run('ls -l ' + dirname)
 
 
-# @task  # 函数装饰器，标识的函数为fab可调用的，非标记的对fab不可见，纯业务逻辑
+# mes库
+def bak32():
+    run('scp -r root@172.16.8.32:/home/mysql/data/*.gz /home/mount-backup/binlog-backup/mes')
+
+
+# report 库
+def bak42():
+    run('scp -r root@172.16.8.42:/home/mysql/data/*.gz /home/mount-backup/binlog-backup/report')
+
+
+# other 库
+def bak37():
+    run('scp -r root@172.16.8.37:/home/mysql/data/*.gz /home/mount-backup/binlog-backup/other')
+
+
+# TODO 执行之前，先去服务器将日志打包
+@task
 def go():
     print("---开始---")
+    bak42()
 
-    workask('/home')
     print('---结束---')
 
 
